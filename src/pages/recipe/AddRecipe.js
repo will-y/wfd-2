22dc887e3851f7b394c71d0d4733c2170b19633e
Will Yelton <willhyelton@gmail.com>
2022-06-02 15:25:32 -0400

    Converts AddRecipe component to a class component

--- src/pages/recipe/AddRecipe.js
+++ src/pages/recipe/AddRecipe.js
@@ -1,37 +1,44 @@
 import {Button, Form} from "react-bootstrap";
+import React from "react";
 
-function AddRecipe() {
-    return (
-        <Form>
-            <Form.Group className="mb-3">
-                <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="Recipe Name"/>
-            </Form.Group>
+class AddRecipe extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-            <Form.Group className="mb-3">
-                <Form.Label>Source</Form.Label>
-                <Form.Control type="text" placeholder="Recipe Source"/>
-            </Form.Group>
+    render = () => {
+        return (
+            <Form>
+                <Form.Group className="mb-3">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" placeholder="Recipe Name"/>
+                </Form.Group>
 
-            <Form.Group className="mb-3">
-                <Form.Select>
-                    <option value="main">Main Course</option>
-                    <option value="side">Side Item</option>
-                    <option value="desert">Desert</option>
-                    <option value="drink">Drink</option>
-                </Form.Select>
-            </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Source</Form.Label>
+                    <Form.Control type="text" placeholder="Recipe Source"/>
+                </Form.Group>
 
-            <Form.Group className="mb-3">
-                <Form.Label>Servings</Form.Label>
-                <Form.Control type="number" placeholder="Recipe Servings"/>
-            </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Select>
+                        <option value="main">Main Course</option>
+                        <option value="side">Side Item</option>
+                        <option value="desert">Desert</option>
+                        <option value="drink">Drink</option>
+                    </Form.Select>
+                </Form.Group>
 
-            <Button variant="primary">
-                Add Recipe
-            </Button>
-        </Form>
-    );
+                <Form.Group className="mb-3">
+                    <Form.Label>Servings</Form.Label>
+                    <Form.Control type="number" placeholder="Recipe Servings"/>
+                </Form.Group>
+
+                <Button variant="primary">
+                    Add Recipe
+                </Button>
+            </Form>
+        );
+    }
 }
 
 export default AddRecipe;
