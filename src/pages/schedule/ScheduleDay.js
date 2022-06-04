cbe12f28eb608658a046738a24ff6607ba8c4d4a
Will Yelton <willhyelton@gmail.com>
2022-06-04 01:34:20 -0400

    Adds a temporary recipe to every day

--- src/pages/schedule/ScheduleDay.js
+++ src/pages/schedule/ScheduleDay.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Schedule.css";
+import {Col, Row} from "react-bootstrap";
 
 class ScheduleDay extends React.Component {
     constructor(props) {
@@ -12,6 +13,23 @@ class ScheduleDay extends React.Component {
                 <div className="p-3">
                     <p>{this.props.day}</p>
                     <p>{this.props.month} {this.props.date}</p>
+                    <Row>
+                        <Col>
+                            <Row className="day-recipe mb-2">
+                                <Col sm={8}>Test Recipe Name</Col>
+                                <Col sm={4}>Servings: 10</Col>
+                            </Row>
+                            <Row className="day-recipe mb-2">
+                                <Col sm={8}>Test Recipe Name</Col>
+                                <Col sm={4}>Servings: 10</Col>
+                            </Row>
+                            <Row className="day-recipe mb-2">
+                                <Col sm={8}>Test Recipe Name</Col>
+                                <Col sm={4}>Servings: 10</Col>
+                            </Row>
+                        </Col>
+                    </Row>
+
                 </div>
 
             </div>
