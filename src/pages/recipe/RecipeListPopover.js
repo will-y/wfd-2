a9c80f44bc4ef0cee91809129c5057550d9c7f31
Will Yelton <willhyelton@gmail.com>
2022-06-04 21:03:09 -0400

    Adds button to add recipe to schedule

--- src/pages/recipe/RecipeListPopover.js
+++ src/pages/recipe/RecipeListPopover.js
@@ -19,12 +19,7 @@ class RecipeListPopover extends React.Component {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <h4>Centered Modal</h4>
-                    <p>
-                        Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                        dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                        consectetur ac, vestibulum at eros.
-                    </p>
+
                 </Modal.Body>
                 <Modal.Footer>
                     <Button onClick={this.props.onHide}>Close</Button>
--- src/pages/schedule/ScheduleDay.js
+++ src/pages/schedule/ScheduleDay.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Schedule.css";
-import {Col, Row} from "react-bootstrap";
+import {Button, Col, Row} from "react-bootstrap";
 
 class ScheduleDay extends React.Component {
     constructor(props) {
@@ -30,6 +30,12 @@ class ScheduleDay extends React.Component {
                         </Col>
                     </Row>
                 </div>
+                <Button variant="success" className="add-recipe-button" onClick={() => this.setState({modalShow: true})}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                         className="bi bi-plus-lg" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+                    </svg>
+                </Button>
             </div>
         );
     }
