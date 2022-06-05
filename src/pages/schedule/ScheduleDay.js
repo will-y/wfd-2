132702289cfdec0a3489dbf4a2015c0b9e5490c8
Will Yelton <willhyelton@gmail.com>
2022-06-04 21:13:37 -0400

    Modal now opens when add button is clicked

--- src/pages/schedule/ScheduleDay.js
+++ src/pages/schedule/ScheduleDay.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./Schedule.css";
 import {Button, Col, Row} from "react-bootstrap";
+import RecipeListPopover from "../recipe/RecipeListPopover";
 
 class ScheduleDay extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            modalShow: false
+        }
     }
 
     render() {
@@ -30,7 +35,10 @@ class ScheduleDay extends React.Component {
                         </Col>
                     </Row>
                 </div>
-                <Button variant="success" className="add-recipe-button" onClick={() => this.setState({modalShow: true})}>
+                <RecipeListPopover show={this.state.modalShow} onHide={() => this.setState({modalShow: false})}/>
+                <Button variant="success"
+                        className="add-recipe-button"
+                        onClick={() => this.setState({modalShow: true})}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
                          className="bi bi-plus-lg" viewBox="0 0 16 16">
                         <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
