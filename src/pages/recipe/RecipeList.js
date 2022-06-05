65cd77810b2e7361987e00b0b7b27e4dfe8ff704
Will Yelton <willhyelton@gmail.com>
2022-06-05 14:47:13 -0400

    Adds functionality to the button to add recipe from list page

--- src/pages/recipe/RecipeList.js
+++ src/pages/recipe/RecipeList.js
@@ -5,6 +5,8 @@ import "./Recipe.css"
 import "../schedule/Schedule.css";
 import {Button, FormControl} from "react-bootstrap";
 import RecipeListEntry from "./RecipeListEntry";
+import {LinkContainer} from 'react-router-bootstrap';
+import {Link} from "react-router-dom";
 
 const categoryOrder = {
     "main": 0,
@@ -128,14 +130,13 @@ class RecipeList extends React.Component {
                                          key={recipe.key} onClick={() => this.handleAddRecipeToDay(recipe.key)}/>
                     );
                 })}
-                <Button variant="success"
-                        className="add-recipe-button"
-                        onClick={() => {}}>
+                <Link to="/add-recipe" role="button"
+                        className="add-recipe-button btn btn-success">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                         className="bi bi-plus-lg" viewBox="0 0 16 16">
+                         className="bi bi-plus-lg link-svg" viewBox="0 0 16 16">
                         <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
                     </svg>
-                </Button>
+                </Link>
             </div>
         );
     }
