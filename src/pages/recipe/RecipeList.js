807dd34abd86b139408dff19c14b224b10051530
Will Yelton <willhyelton@gmail.com>
2023-04-19 21:21:52 -0400

    Oops

--- src/pages/recipe/RecipeList.js
+++ src/pages/recipe/RecipeList.js
@@ -123,7 +123,7 @@ class RecipeList extends React.Component {
                     );
                 })}
                 <Link to="/add" role="button"
-                        className="add-recipe-button btn btn-success">
+                        className="add-recipe-button btn btn-success global-add-recipe-button">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
                          className="bi bi-plus-lg link-svg" viewBox="0 0 16 16">
                         <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
