a48cb8e9d9887ed73932419ad1e3cf2d8aa61607
Will Yelton <willhyelton@gmail.com>
2023-06-01 17:49:33 -0400

    Adds no access page

--- src/components/pages/other/NoAccess.js
+++ src/components/pages/other/NoAccess.js
@@ -1,6 +1,22 @@
 import {Container} from "react-bootstrap";
+import {useEffect} from "react";
+import {get, ref} from "firebase/database";
+import {auth, database} from "../../../firebase";
+import {useNavigate} from "react-router-dom";
 
 export function NoAccess() {
+    const navigate = useNavigate();
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            const userRef = ref(database, "/users/" + user.uid);
+
+            get(userRef).then(snapshot => {
+                if (snapshot.exists() && (snapshot.val() === "admin" || snapshot.val() === "user")) {
+                    navigate('/');
+                }
+            });
+        });
+    }, [navigate]);
     return (
         <Container className="text-center mt-4">
             <h1>No Access</h1>
